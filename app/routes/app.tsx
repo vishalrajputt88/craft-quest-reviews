faff74f7999758b899d88057bcfe12cfb38dabd5
Vaibhav <eventvaibhav@gmail.com>
2026-09-21 18:57:32 +0530

    add file scopes

--- app/routes/app.tsx
+++ app/routes/app.tsx
@@ -20,12 +20,14 @@ export default function App() {
 
   return (
     <AppProvider isEmbeddedApp apiKey={apiKey}>
-      <NavMenu>
-        <Link to="/app" rel="home">
-          Home
-        </Link>
-        <Link to="/app/additional">Additional page</Link>
-      </NavMenu>
+     <NavMenu>
+  <Link to="/app" rel="home">
+    Home
+  </Link>
+  <Link to="/app">Reviews</Link>
+  <Link to="/app/frames">Frames</Link>
+  <Link to="/app/settings">Settings</Link>
+</NavMenu>
       <Outlet />
     </AppProvider>
   );
